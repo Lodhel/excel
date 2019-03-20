62f7c1c789636b5e3733474cb0c72ad24b50c9ca
lodhel <lodhelnew@gmail.com>
2019-03-21 00:51:03 +0300

    передал каждую колонку в словарь

--- src/main.py
+++ src/main.py
@@ -1,13 +1,35 @@
 import openpyxl
 import os
+import numpy
 
-my_direct = os.path.dirname(os.path.abspath(__file__))
-workbook = openpyxl.load_workbook('{}\prices.xlsx'.format(my_direct))
 
-def read():
-    worksheet = workbook.get_sheet_by_name('Sheet1')
+class Excel_py:
+    my_direct = os.path.dirname(os.path.abspath(__file__))
+    workbook = openpyxl.load_workbook('{}\prices.xlsx'.format(my_direct))
 
-    worksheet.iter_cols(min_row=1, min_col=1)
-    worksheet['A1']
+    def read(self, min_row=1, min_col=1, worksheet_name='A1'):
+        worksheet = self.workbook.get_sheet_by_name('Sheet1')
+        worksheet.iter_cols(min_row=min_row, min_col=min_col)
 
-print(1)
+        return worksheet[worksheet_name]
+
+    def get_data(self):
+        data = {
+            'A': [self.read(worksheet_name='A{}'.format(index+1)).value for index in range(251)],
+            'B': [self.read(worksheet_name='B{}'.format(index+1)).value for index in range(251)],
+            'C': [self.read(worksheet_name='C{}'.format(index+1)).value for index in range(251)],
+            'D': [self.read(worksheet_name='D{}'.format(index+1)).value for index in range(251)],
+            'E': [self.read(worksheet_name='E{}'.format(index+1)).value for index in range(251)],
+            'F': [self.read(worksheet_name='F{}'.format(index+1)).value for index in range(251)],
+            'G': [self.read(worksheet_name='G{}'.format(index+1)).value for index in range(251)],
+            'H': [self.read(worksheet_name='H{}'.format(index+1)).value for index in range(251)],
+            'I': [self.read(worksheet_name='I}'.format(index+1)).value for index in range(251)],
+            'J': [self.read(worksheet_name='J{}'.format(index+1)).value for index in range(251)],
+            'K': [self.read(worksheet_name='K{}'.format(index+1)).value for index in range(251)],
+            'L': [self.read(worksheet_name='L{}'.format(index+1)).value for index in range(251)],
+        }
+
+        return data
+
+
+# = numpy.mean()
