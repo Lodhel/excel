3fe3f3b43ab5c054de5f7568658537c0fb502bd7
lodhel <lodhelnew@gmail.com>
2019-03-20 23:47:11 +0300

    чтение файла

--- src/main.py
+++ src/main.py
@@ -2,8 +2,12 @@ import openpyxl
 import os
 
 my_direct = os.path.dirname(os.path.abspath(__file__))
-
 workbook = openpyxl.load_workbook('{}\prices.xlsx'.format(my_direct))
-worksheet = workbook.get_sheet_by_name('Sheet1')
+
+def read():
+    worksheet = workbook.get_sheet_by_name('Sheet1')
+
+    worksheet.iter_cols(min_row=1, min_col=1)
+    worksheet['A1']
 
 print(1)
